1473d49674c76f853f8f3e5ae2ca11505c9a241a
NyeongC <cjfsud23@naver.com>
2024-09-08 22:56:20 +0900

    비동기 테스트 @EnableAsync - 클래스 @Async - 메서드

--- mylo/src/main/java/com/ccn/mylo/scheduler/ScheduledTasks.java
+++ mylo/src/main/java/com/ccn/mylo/scheduler/ScheduledTasks.java
@@ -2,19 +2,25 @@ package com.ccn.mylo.scheduler;
 
 import com.ccn.mylo.Service.LottoMatch;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @EnableScheduling
+@EnableAsync
 public class ScheduledTasks {
 
     @Autowired
     LottoMatch lottoMatch;
 
-    @Scheduled(cron = "*/30 * * * * *") // 30초마다 실행
+    @Async
+    @Scheduled(cron = "*/10 * * * * *") // 30초마다 실행
     public void printMessage() {
+        
+        System.out.println("비동기 스레드 시작");
         lottoMatch.lottoMatch();
     }
 }
